74da29696413ef93227ac9b02f8aeae4314ea034
CaptainCoding7 <paulagopian94@gmail.com>
2022-06-15 18:19:43 +0200

    Implemented wrapper functions for TM pkt

--- src/ccsds/CCSDS_Pkt.h
+++ src/ccsds/CCSDS_Pkt.h
@@ -144,10 +144,15 @@ private:
 /*************************  API - C/C++ functions  *******************/
 
 typedef void* CCSDS_PKT;
+typedef void* CCSDS_PKT_TC;
+typedef void* CCSDS_PKT_TM;
 typedef void* SPW_HDR;
 typedef void* PRIM_HDR;
 typedef void* PKT_DATA;
+typedef void* PKT_DATA_TC;
+typedef void* PKT_DATA_TM;
 typedef void* SEC_HDR_TC;
+typedef void* SEC_HDR_TM;
 
 typedef void* IDFIELD;
 typedef void* COUNTERFIELD;
@@ -176,36 +181,55 @@ void test_create_CCSDS_Pkt();
 
 /*** Creation of C++ objects (+return) ***/
 CCSDS_PKT create_CCSDS_Pkt_TC(unsigned char dest_port_addr);
+CCSDS_PKT create_CCSDS_Pkt_TM(unsigned char dest_port_addr);
 
 /// delete
 void delete_CCSDS_Pkt(CCSDS_PKT ccsds_pkt, int no);
 
 
 /*** Getter calls ***/
+
 /// CCSDS_Pkt
 SPW_HDR call_CCSDS_Pkt_get_spw_hdr(CCSDS_PKT ccsds_pkt);
 PRIM_HDR call_CCSDS_Pkt_get_prim_hdr(CCSDS_PKT ccsds_pkt);
-PKT_DATA call_CCSDS_Pkt_get_pkt_data(CCSDS_PKT ccsds_pkt);
+PKT_DATA_TC call_CCSDS_Pkt_TC_get_pkt_data(CCSDS_PKT_TC ccsds_pkt);
+PKT_DATA_TM call_CCSDS_Pkt_TM_get_pkt_data(CCSDS_PKT_TM ccsds_pkt);
+
 /// SPW_HDR
 unsigned char call_Spw_hdr_get_addr(SPW_HDR spw_hdr);
 unsigned char call_Spw_hdr_get_protid(SPW_HDR spw_hdr);
 unsigned char call_Spw_hdr_get_spare(SPW_HDR spw_hdr);
 unsigned char call_Spw_hdr_get_user_app(SPW_HDR spw_hdr);
+
 /// PRIM_HDR
 enum Id call_Prim_hdr_get_id(PRIM_HDR prim_hdr);
 uint16_t call_Prim_hdr_get_counter(PRIM_HDR prim_hdr);
 uint16_t call_Prim_hdr_get_len(PRIM_HDR prim_hdr);
-/// CCSDS_PKT_DATA
-SEC_HDR_TC call_Pkt_data_get_sec_hdr(PKT_DATA pkt_data);
-uint8_t *call_Pkt_data_get_app_data(PKT_DATA pkt_data);
-uint16_t call_Pkt_data_get_crc(PKT_DATA pkt_data);
-/// SEC_HDR
-int call_Sec_hdr_get_pus_version(SEC_HDR_TC sec_hdr);
-uint8_t call_Sec_hdr_get_ackflag(SEC_HDR_TC sec_hdr);
-uint8_t call_Sec_hdr_get_serviceType(SEC_HDR_TC sec_hdr);
-uint8_t call_Sec_hdr_get_serviceSubType(SEC_HDR_TC sec_hdr);
-uint16_t call_Sec_hdr_get_sourceId(SEC_HDR_TC sec_hdr);
-uint8_t call_Sec_hdr_get_spare(SEC_HDR_TC sec_hdr);
+
+/// CCSDS_PKT_DATA_TC
+SEC_HDR_TC call_Pkt_data_TC_get_sec_hdr(PKT_DATA_TC pkt_data);
+uint8_t *call_Pkt_data_TC_get_app_data(PKT_DATA_TC pkt_data);
+uint16_t call_Pkt_data_TC_get_crc(PKT_DATA_TC pkt_data);
+/// CCSDS_PKT_DATA_TC
+SEC_HDR_TM call_Pkt_data_TM_get_sec_hdr(PKT_DATA_TM pkt_data);
+uint8_t *call_Pkt_data_TM_get_app_data(PKT_DATA_TM pkt_data);
+uint16_t call_Pkt_data_TM_get_crc(PKT_DATA_TM pkt_data);
+
+/// SEC_HDR_TC
+int call_Sec_hdr_TC_get_pus_version(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TC_get_ackflag(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TC_get_serviceType(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TC_get_serviceSubType(SEC_HDR_TC sec_hdr);
+uint16_t call_Sec_hdr_TC_get_sourceId(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TC_get_spare(SEC_HDR_TC sec_hdr);
+/// SEC_HDR_TM
+int call_Sec_hdr_TM_get_pus_version(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TM_get_scTimeRefStatus(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TM_get_serviceType(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TM_get_serviceSubType(SEC_HDR_TC sec_hdr);
+uint16_t call_Sec_hdr_TM_get_msgTypeCounter(SEC_HDR_TC sec_hdr);
+uint16_t call_Sec_hdr_TM_get_destId(SEC_HDR_TC sec_hdr);
+uint8_t call_Sec_hdr_TM_get_spare(SEC_HDR_TC sec_hdr);
 
 #ifdef __cplusplus
 }
--- src/ccsds/CCSDS_Pkt_Test.cpp
+++ src/ccsds/CCSDS_Pkt_Test.cpp
@@ -91,12 +91,16 @@ extern "C" PRIM_HDR call_CCSDS_Pkt_get_prim_hdr(CCSDS_PKT ccsds_pkt)
 	auto pccsds_pkt = reinterpret_cast<CCSDS_Pkt*>(ccsds_pkt);
 	return pccsds_pkt->get_prim_hdr();
 }
-extern "C" PKT_DATA call_CCSDS_Pkt_get_pkt_data(CCSDS_PKT ccsds_pkt)
+extern "C" PKT_DATA_TC call_CCSDS_Pkt_TC_get_pkt_data(CCSDS_PKT_TC ccsds_pkt)
 {
 	auto pccsds_pkt = reinterpret_cast<CCSDS_Pkt_TC*>(ccsds_pkt);
+	return pccsds_pkt->get_pkt_data():
+}
+extern "C" PKT_DATA_TM call_CCSDS_Pkt_TM_get_pkt_data(CCSDS_PKT_TM ccsds_pkt)
+{
+	auto pccsds_pkt = reinterpret_cast<CCSDS_Pkt_TM*>(ccsds_pkt);
 	return pccsds_pkt->get_pkt_data();
 }
-
 
 /// Spw_hdr -------------------
 extern "C" unsigned char call_Spw_hdr_get_addr(SPW_HDR spw_hdr)
@@ -141,61 +145,107 @@ extern "C" uint16_t call_Prim_hdr_get_len(PRIM_HDR prim_hdr)
 }
 
 
-/// CCSDS_Pkt_data ----------------
-extern "C" SEC_HDR_TC call_Pkt_data_get_sec_hdr(PKT_DATA pkt_data)
+/// CCSDS_Pkt_data_TC ----------------
+extern "C" SEC_HDR_TC call_Pkt_data_TC_get_sec_hdr(PKT_DATA_TC pkt_data)
 {
 	auto ppkt_data = reinterpret_cast<Pkt_data_TC*>(pkt_data);
 	return ppkt_data->getMSec_hdr_tc();
 }
-extern "C" uint8_t *call_Pkt_data_get_app_data(PKT_DATA pkt_data)
+extern "C" uint8_t *call_Pkt_data_TC_get_app_data(PKT_DATA_TC pkt_data)
 {
-	auto ppkt_data = reinterpret_cast<Pkt_data<APP_DATA_TC_SIZE>*>(pkt_data);
+	auto ppkt_data = reinterpret_cast<Pkt_data_TC*>(pkt_data);
+	return ppkt_data->getMData():
+}
+extern "C" uint16_t call_Pkt_data_TC_get_crc(PKT_DATA_TC pkt_data)
+{
+	auto ppkt_data = reinterpret_cast<Pkt_data_TC>(pkt_data);
+	return ppkt_data->getMCrc();
+}
+/// CCSDS_Pkt_data_TM ----------------
+extern "C" SEC_HDR_TM call_Pkt_data_TM_get_sec_hdr(PKT_DATA_TM pkt_data)
+{
+	auto ppkt_data = reinterpret_cast<Pkt_data_TM*>(pkt_data);
+	return ppkt_data->getMSec_hdr_tm();
+}
+extern "C" uint8_t *call_Pkt_data_TM_get_app_data(PKT_DATA_TM pkt_data)
+{
+	auto ppkt_data = reinterpret_cast<Pkt_data_TM*>(pkt_data);
 	return ppkt_data->getMData();
 }
-extern "C" uint16_t call_Pkt_data_get_crc(PKT_DATA pkt_data)
+extern "C" uint16_t call_Pkt_data_TM_get_crc(PKT_DATA_TM pkt_data)
 {
-	auto ppkt_data = reinterpret_cast<Pkt_data<APP_DATA_TC_SIZE>*>(pkt_data);
+	auto ppkt_data = reinterpret_cast<Pkt_data_TM*>(pkt_data);
 	return ppkt_data->getMCrc();
 }
 
-
 /// Sec_hdr_TC -----------------------
 
-extern "C" int call_Sec_hdr_get_pus_version(SEC_HDR_TC sec_hdr)
+extern "C" int call_Sec_hdr_TC_get_pus_version(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMAck();
 	//printf("size of sec_hdr = %d\n", sizeof(psec_hdr));
-	return psec_hdr->m_version();
+	return psec_hdr->m_pus_version();
 }
-extern "C" uint8_t call_Sec_hdr_get_ackflag(SEC_HDR_TC sec_hdr)
+extern "C" uint8_t call_Sec_hdr_TC_get_ackflag(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMAck();
 	return psec_hdr->m_ackFlags();
 }
-extern "C" uint8_t call_Sec_hdr_get_serviceType(SEC_HDR_TC sec_hdr)
+extern "C" uint8_t call_Sec_hdr_TC_get_serviceType(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMServiceType();
 	return psec_hdr->m_serviceType();
 }
-extern "C" uint8_t call_Sec_hdr_get_serviceSubType(SEC_HDR_TC sec_hdr)
+extern "C" uint8_t call_Sec_hdr_TC_get_serviceSubType(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMServiceSubType();
 	return psec_hdr->m_serviceSubType();
 }
-extern "C" uint16_t call_Sec_hdr_get_sourceId(SEC_HDR_TC sec_hdr)
+extern "C" uint16_t call_Sec_hdr_TC_get_sourceId(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMSourceId();
 	return psec_hdr->m_sourceId();
 }
-extern "C" uint8_t call_Sec_hdr_get_spare(SEC_HDR_TC sec_hdr)
+extern "C" uint8_t call_Sec_hdr_TC_get_spare(SEC_HDR_TC sec_hdr)
 {
 	auto psec_hdr = reinterpret_cast<Sec_hdr_TC*>(sec_hdr);
-	//return psec_hdr->getMSpare();
 	return psec_hdr->m_spare();
 }
 
+/// Sec_hdr_TM -----------------------
+
+extern "C" int call_Sec_hdr_TM_get_pus_version(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_pus_version();
+}
+extern "C" int call_Sec_hdr_TM_get_scTimeRefStatus(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_scTimeRefStatus();
+}
+extern "C" uint8_t call_Sec_hdr_TM_get_serviceType(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_serviceType();
+}
+extern "C" uint8_t call_Sec_hdr_TM_get_serviceSubType(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_serviceSubType();
+}
+extern "C" uint16_t call_Sec_hdr_TM_get_msgTypeCounter(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_msgTypeCounter();
+}
+extern "C" uint16_t call_Sec_hdr_TM_get_sourceId(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_destId();
+}
+extern "C" uint8_t call_Sec_hdr_TM_get_spare(SEC_HDR_TM sec_hdr)
+{
+	auto psec_hdr = reinterpret_cast<Sec_hdr_TM*>(sec_hdr);
+	return psec_hdr->m_spare();
+}
